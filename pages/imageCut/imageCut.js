f82ab2b4f23a862c07566eda0312aca20b81d900
‘JefferyLiang <liangxingyu@jingdaka.com>
2018-11-19 16:36:16 +0800

    lxy:fix(图片无法保存到本地问题)

--- pages/imageCut/imageCut.js
+++ pages/imageCut/imageCut.js
@@ -9,6 +9,30 @@ Page({
     })
   },
   saveImg(e){  
+    const that = this;
+    wx.showToast({
+      title: '正在保存',
+      icon: 'success',
+      duration: 6000
+    })
+    //保存到系统相册
+    wx.saveImageToPhotosAlbum({
+      filePath: e.detail,
+      success(res) {
+        wx.showToast({
+          title: '成功',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail(err) {
+        wx.hideToast()
+        wx.showModal({
+          title: '提示',
+          content: '图片保存失败,请尝试重新保存:' + err.errMsg,
+        })
+      }
+    })
     wx.redirectTo({
       url: `../test/test?imageSrc=${e.detail}`
     })
